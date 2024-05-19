275f6066370b1edc68120421cca12dfc58fde1e4
Icyatwa <icyatwandoba@gmail.com>
2024-05-19 01:30:56 -1100

    logic

--- controllers/rideBookingController.js
+++ controllers/rideBookingController.js
@@ -1,26 +1,30 @@
-// // rideBookingController.js
+// const mongoose = require('mongoose');
+// const Ride = require('../models/rideModel');
 // const RideBooking = require('../models/rideBookingModel');
 // const BusDetails = require('../models/busDetailsModel');
-// const Ride = require('../models/rideModel');
 
 // const bookRide = async (req, res) => {
 //   const { ride_id, seatsBooked } = req.body;
+//   const session = await mongoose.startSession();
+//   session.startTransaction();
+
 //   try {
-//     const ride = await Ride.findById(ride_id);
+//     const ride = await Ride.findById(ride_id).session(session);
 //     if (!ride) {
+//       await session.abortTransaction();
+//       session.endSession();
 //       return res.status(404).json({ error: 'Ride not found' });
 //     }
 
-//     // Book the ride
-//     const booking = await RideBooking.create({
-//       ride: ride_id,
-//       seatsBooked
-//     });
+//     const booking = await RideBooking.create([{ 
+//       ride: ride_id, 
+//       seatsBooked 
+//     }], { session });
 
-//     // Update bus details for the specific segments
-//     const busDetails = await BusDetails.findOne({ rideGroupId: ride.rideGroupId });
-
+//     const busDetails = await BusDetails.findOne({ rideGroupId: ride.rideGroupId }).session(session);
 //     if (!busDetails) {
+//       await session.abortTransaction();
+//       session.endSession();
 //       return res.status(404).json({ error: 'Bus details not found' });
 //     }
 
@@ -29,32 +33,47 @@
 
 //     const segmentsToUpdate = new Set();
 
-//     // Collect segments with the same starting point
-//     const ridesWithSameStart = await Ride.find({ rideGroupId: ride.rideGroupId, 'stations.0': startStation });
+//     // Find all segments that start at the same station
+//     const ridesWithSameStart = await Ride.find({ 
+//       rideGroupId: ride.rideGroupId, 
+//       'stations.0': startStation 
+//     }).session(session);
 //     ridesWithSameStart.forEach(ride => {
 //       segmentsToUpdate.add(`${ride.stations[0]}-${ride.stations[1]}`);
 //     });
 
-//     // Collect segments with the same ending point
-//     const ridesWithSameEnd = await Ride.find({ rideGroupId: ride.rideGroupId, 'stations.1': endStation });
+//     // Find all segments that end at the same station
+//     const ridesWithSameEnd = await Ride.find({ 
+//       rideGroupId: ride.rideGroupId, 
+//       'stations.1': endStation 
+//     }).session(session);
 //     ridesWithSameEnd.forEach(ride => {
 //       segmentsToUpdate.add(`${ride.stations[0]}-${ride.stations[1]}`);
 //     });
 
-//     // Update capacities for all relevant segments
 //     segmentsToUpdate.forEach(segmentKey => {
 //       if (!busDetails.segmentCapacities.has(segmentKey)) {
 //         busDetails.segmentCapacities.set(segmentKey, busDetails.busCapacity);
 //       }
 
-//       const updatedCapacity = busDetails.segmentCapacities.get(segmentKey) - seatsBooked;
+//       const currentCapacity = busDetails.segmentCapacities.get(segmentKey);
+//       const updatedCapacity = currentCapacity - seatsBooked;
+
+//       if (updatedCapacity < 0) {
+//         throw new Error('Not enough capacity available for the booking');
+//       }
+
 //       busDetails.segmentCapacities.set(segmentKey, updatedCapacity);
 //     });
 
-//     await busDetails.save();
+//     await busDetails.save({ session });
+//     await session.commitTransaction();
+//     session.endSession();
 
 //     res.status(201).json({ booking, updatedBusDetails: busDetails });
 //   } catch (error) {
+//     await session.abortTransaction();
+//     session.endSession();
 //     console.error('Error booking ride:', error);
 //     res.status(500).json({ error: 'Internal Server Error' });
 //   }
@@ -104,7 +123,10 @@ const bookRide = async (req, res) => {
       'stations.0': startStation 
     }).session(session);
     ridesWithSameStart.forEach(ride => {
-      segmentsToUpdate.add(`${ride.stations[0]}-${ride.stations[1]}`);
+      // Ensure segment direction matches
+      if (ride.stations[1] !== endStation) {
+        segmentsToUpdate.add(`${ride.stations[0]}-${ride.stations[1]}`);
+      }
     });
 
     // Find all segments that end at the same station
@@ -113,7 +135,10 @@ const bookRide = async (req, res) => {
       'stations.1': endStation 
     }).session(session);
     ridesWithSameEnd.forEach(ride => {
-      segmentsToUpdate.add(`${ride.stations[0]}-${ride.stations[1]}`);
+      // Ensure segment direction matches
+      if (ride.stations[0] !== startStation) {
+        segmentsToUpdate.add(`${ride.stations[0]}-${ride.stations[1]}`);
+      }
     });
 
     segmentsToUpdate.forEach(segmentKey => {
@@ -145,5 +170,3 @@ const bookRide = async (req, res) => {
 };
 
 module.exports = { bookRide };
-
-
